20445d5869a3ed8593c1e61b3157a6d7011d3c43
AlexDoblyno <alexdoblyno@gmail.com>
2025-03-23 18:16:44 -0600

    Changes

--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -19,3 +19,27 @@ public class GameUI {
     private WebSocketClient webSocketClient;
     private ChessGame chessGame;
     private static boolean closed = false;
+
+    public GameUI(Client client, Integer gameID, ChessGame.TeamColor teamColor, String authToken) throws Exception {
+        this.client = client;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.scanner = new Scanner(System.in);
+        this.color = teamColor;
+        try {
+            this.webSocketClient = client.getServerFacade().createWebSocketClient(this);
+        } catch (Exception e) {
+            throw new Exception("Error creating web socket client");
+        }
+        try {
+            UserGameCommand joinCommand = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            webSocketClient.sendMessage(joinCommand);
+        } catch (Exception e) {
+            throw new Exception("Error joining web socket server");
+        }
+        if (teamColor == null) {
+            webSocketClient.teamColor = ChessGame.TeamColor.WHITE;
+        } else {
+            webSocketClient.teamColor = teamColor;
+        }
+    }
